9a5b5cc75d34ada64a40d19f05062eda728e2dcb
CozyBrian <dragonbrian33@gmail.com>
2022-07-01 18:10:40 +0000

    add: settings and viewport window

--- Ray-Tracing-II/src/WalnutApp.cpp
+++ Ray-Tracing-II/src/WalnutApp.cpp
@@ -3,28 +3,64 @@
 
 #include "Walnut/Image.h"
 
+using namespace Walnut;
+
 class ExampleLayer : public Walnut::Layer
 {
 public:
 	virtual void OnUIRender() override
 	{
-		ImGui::Begin("Hello");
-		ImGui::Button("Button");
+		ImGui::Begin("Settings");
+		if (ImGui::Button("Render"))
+		{
+			Render();
+		}
 		ImGui::End();
 
-		ImGui::ShowDemoWindow();
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("ViewPort");
+		nViewpotWidth = ImGui::GetContentRegionAvail().x;
+		nViewpotHeight = ImGui::GetContentRegionAvail().y;
+
+		if (m_Image)
+			ImGui::Image(m_Image->GetDescriptorSet(), {(float)m_Image->GetWidth(), (float)m_Image->GetHeight()});
+
+		ImGui::End();
+		ImGui::PopStyleVar();
 	}
+
+	void Render()
+	{
+		if (!m_Image || nViewpotWidth != m_Image->GetWidth() || nViewpotHeight != m_Image->GetHeight())
+		{
+			m_Image = std::make_shared<Image>(nViewpotWidth, nViewpotHeight, ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[nViewpotWidth * nViewpotHeight];
+		}
+
+		for (uint32_t i = 0; i < nViewpotWidth * nViewpotHeight; i++)
+		{
+			m_ImageData[i] = 0xffff00ff;
+		}
+
+		m_Image->SetData(m_ImageData);
+	}
+
+private:
+	std::shared_ptr<Image> m_Image;
+	uint32_t nViewpotWidth = 0, nViewpotHeight = 0;
+	uint32_t* m_ImageData = nullptr;
 };
 
-Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+Walnut::Application *Walnut::CreateApplication(int argc, char **argv)
 {
 	Walnut::ApplicationSpecification spec;
 	spec.Name = "Ray Tracing";
 
-	Walnut::Application* app = new Walnut::Application(spec);
+	Walnut::Application *app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
 	app->SetMenubarCallback([app]()
-	{
+													{
 		if (ImGui::BeginMenu("File"))
 		{
 			if (ImGui::MenuItem("Exit"))
@@ -32,7 +68,6 @@ Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 				app->Close();
 			}
 			ImGui::EndMenu();
-		}
-	});
+		} });
 	return app;
 }
